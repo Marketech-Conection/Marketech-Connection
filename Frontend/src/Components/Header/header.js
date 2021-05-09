57a380ed087474d9847315bfdd1cff4a3119c4fd
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2021-05-09 17:09:38 -0300

    Melhoria na navbar

--- Frontend/src/Components/Header/header.js
+++ Frontend/src/Components/Header/header.js
@@ -19,7 +19,13 @@ function Header(){
                     </div>
                     <li><Link to="/salvos"><FaHeart/></Link></li>
                     <li><Link href="/"><FaUser/></Link></li>
-                    <li><Link to={`/Carrinho`}><FaShoppingCart/></Link></li>
+                    <li>
+                        <Link to='/Carrinho'>
+                            <div className="num-products">
+                                <p className="shop-icon"><FaShoppingCart/><span>0</span></p>
+                            </div>
+                        </Link>
+                    </li>
                 </ul>
         </nav>
         </header>
